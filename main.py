7b4c3a625245f5d760429fbd120bb63296bcf72f
NakkiiBrks <53010446+NakkiiBrks@users.noreply.github.com>
2024-08-10 04:11:07 +0000

    Basic app command

--- main.py
+++ main.py
@@ -3,19 +3,34 @@ import os
 import discord
 from discord.ext import commands
 
-intents = discord.Intents.all()
-intents.members = True
-intents.message_content = True
-
 presence = discord.Game(name='com a Madoka')
 
-bot = commands.Bot(command_prefix='?', activity=presence, intents=intents)
+bot = commands.Bot(command_prefix='?',
+                   activity=presence,
+                   intents=discord.Intents.all())
 
 
 @bot.event
 async def on_ready():
     print(f'Logado como: {bot.user}')
     print('------------------------')
+    await bot.tree.sync()
+
+
+"""
+# Comando base old
+
+@bot.command()
+async def teste1(ctx):
+    await ctx.send('teste2')
+"""
+
+
+# App command teste
+@bot.tree.command(name='teste-input',
+                  description='Apenas um teste de app command')
+async def testeinput(interaction: discord.Interaction):
+    await interaction.response.send_message('Teste')
 
 
 token = os.environ['TOKEN']
