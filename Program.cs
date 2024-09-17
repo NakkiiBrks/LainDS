7ec2177c3dad64f48d9cf86c6d2f98cc77e031cf
NakkiiBrks <nakkii800br@gmail.com>
2024-09-16 23:17:12 -0300

    Basic slash command implementation

--- Program.cs
+++ Program.cs
@@ -1,56 +1,94 @@
 ﻿using Discord;
 using Discord.Commands;
+using Discord.Net;
 using Discord.WebSocket;
+using Newtonsoft.Json;
 
-
-public class Program
+namespace LainDS
 {
-#pragma warning disable CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere adicionar o modificador "obrigatório" ou declarar como anulável.
-    private static DiscordSocketClient _client;
-#pragma warning restore CS8618 
-
-    public static async Task Main(string[] args)
+    public class Program
     {
-        _client = new DiscordSocketClient();
+        public static DiscordSocketClient? client;
 
-        _client.Log += Log;
-
-        var token = Environment.GetEnvironmentVariable("TOKEN");
-
-        await _client.LoginAsync(TokenType.Bot, token);
-        await _client.StartAsync();
-
-        await Task.Delay(-1);
-    }
-
-    private static Task Log(LogMessage msg)
-    {
-        Console.WriteLine(msg.ToString());
-        return Task.CompletedTask;
-    }
-}
-
-public class LoggingService
-{
-    LoggingService(DiscordSocketClient client, CommandService command)
-    {
-        client.Log += LogAsync;
-        command.Log += LogAsync;
-    }
-
-    private Task LogAsync(LogMessage message)
-    {
-        if (message.Exception is CommandException cmdException)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine($"[Command/{message.Severity}] {cmdException.Command.Aliases.First()}" +
-                $" failed to execute in {cmdException.Context.Channel}.");
-            Console.WriteLine(cmdException);
-        }
-        else
-        {
-            Console.WriteLine($"[General/{message.Severity}] {message}");
+            client = new DiscordSocketClient();
+            client.Log += Log;
+            client.Ready += Client_Ready;
+            client.SlashCommandExecuted += SlashCommandHandler;
+
+
+            var token = Environment.GetEnvironmentVariable("TOKEN");
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+            await Task.Delay(-1);
         }
 
-        return Task.CompletedTask;
+        public static async Task Client_Ready()
+        {
+            var globalCommand = new SlashCommandBuilder();
+            globalCommand.WithName("idiot");
+            globalCommand.WithDescription("Terry Davis quote");
+
+            try
+            {
+                await client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
+            }
+            catch (HttpException exception)
+            {
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+
+                Console.WriteLine(json);
+            }
+        }
+
+        private static async Task SlashCommandHandler(SocketSlashCommand command)
+        {
+            switch (command.Data.Name)
+            {
+                case "idiot":
+                    await IdiotTerryDavisQuote(command);
+                    break;
+            }
+        }
+
+        private static async Task IdiotTerryDavisQuote(SocketSlashCommand command)
+        {
+            await command.RespondAsync("An idiot admires complexity, a genius admires simplicity, " +
+                "a physicist tries to make it simple, for an idiot anything the more complicated it is the more he will admire it, " +
+                "if you make something so clusterfucked he can't understand it he's gonna think you're a god cause you made it so complicated nobody can understand it. " +
+                "That's how they write journals in Academics, they try to make it so complicated people think you're a genius.");
+        }
+
+        private static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        public class LoggingService
+        {
+            LoggingService(DiscordSocketClient client, CommandService command)
+            {
+                client.Log += LogAsync;
+                command.Log += LogAsync;
+            }
+
+            private Task LogAsync(LogMessage message)
+            {
+                if (message.Exception is CommandException cmdException)
+                {
+                    Console.WriteLine($"[Command/{message.Severity}] {cmdException.Command.Aliases[0]}" +
+                        $" failed to execute in {cmdException.Context.Channel}.");
+                    Console.WriteLine(cmdException);
+                }
+                else
+                {
+                    Console.WriteLine($"[General/{message.Severity}] {message}");
+                }
+
+                return Task.CompletedTask;
+            }
+        }
     }
 }
