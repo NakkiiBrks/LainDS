ebda26d6d6e859d297b18b5c5aa38dfc06434afc
NakkiiBrks <nakkii800br@gmail.com>
2024-09-22 00:32:45 -0300

    [Add]: Basic commands

---
+++ Modules/ExampleModule.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.Interactions;
+using InteractionFramework.Attributes;
+using System;
+using System.Threading.Tasks;
+
+namespace InteractionFramework.Modules;
+
+public class Administration : InteractionModuleBase<SocketInteractionContext>
+{
+    public InteractionService Commands { get; set; }
+
+    private InteractionHandler _handler;
+
+    public Administration(InteractionHandler handler)
+    {
+        _handler = handler;
+    }
+
+    [SlashCommand("terry-quote", "Terry Davis quote")]
+    public async Task TerryQuote()
+        => await RespondAsync("An idiot admires complexity, a genius admires simplicity, " +
+                "a physicist tries to make it simple, for an idiot anything the more complicated it is the more he will admire it, " +
+                "if you make something so clusterfucked he can't understand it he's gonna think you're a god cause you made it so complicated nobody can understand it. " +
+                "That's how they write journals in Academics, they try to make it so complicated people think you're a genius.");
+
+
+    [SlashCommand("ping", "Pings the bot and returns its latency.")]
+    public async Task GreetUserAsync()
+        => await RespondAsync(text: $":ping_pong: It took me {Context.Client.Latency}ms to respond to you!", ephemeral: true);
+}
