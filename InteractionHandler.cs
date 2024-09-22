9c88602910c3a511d2719a3f7adcfb5885e11e79
NakkiiBrks <nakkii800br@gmail.com>
2024-09-21 23:39:16 -0300

    [Dev]: Refactor of base code according to Discord.Net documentation.

---
+++ InteractionHandler.cs
@@ -0,0 +1,95 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace InteractionFramework;
+
+public class InteractionHandler
+{
+    private readonly DiscordSocketClient _client;
+    private readonly InteractionService _handler;
+    private readonly IServiceProvider _services;
+    private readonly IConfiguration _configuration;
+
+    public InteractionHandler(DiscordSocketClient client, InteractionService handler, IServiceProvider services, IConfiguration configuration)
+    {
+        _client = client;
+        _handler = handler;
+        _services = services;
+        _configuration = configuration;
+    }
+
+    public async Task InitializeAsync()
+    {
+        _client.Ready += ReadyAsync;
+        _handler.Log += LogAsync;
+
+        await _handler.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+
+        _client.InteractionCreated += HandleInteraction;
+        _handler.InteractionExecuted += HandleInteractionExecute;
+    }
+
+    private Task LogAsync(LogMessage log)
+    {
+        Console.WriteLine(log);
+        return Task.CompletedTask;
+    }
+
+    private async Task ReadyAsync()
+    {
+        await _handler.RegisterCommandsGloballyAsync();
+    }
+
+    private async Task HandleInteraction(SocketInteraction interaction)
+    {
+        try
+        {
+            var context = new SocketInteractionContext(_client, interaction);
+
+            var result = await _handler.ExecuteCommandAsync(context, _services);
+
+            if (!result.IsSuccess)
+                switch (result.Error)
+                {
+                    case InteractionCommandError.UnmetPrecondition:
+                        await context.Interaction.RespondAsync("You dont have enough permissions to execute this command.");
+                        break;
+                    default:
+                        await context.Interaction.RespondAsync("An error has ocurred when processing this command.");
+                        break;
+                }
+        }
+        catch
+        {
+            if (interaction.Type is InteractionType.ApplicationCommand)
+                await interaction.GetOriginalResponseAsync().ContinueWith(async (msg) => await msg.Result.DeleteAsync());
+        }
+    }
+
+    private Task HandleInteractionExecute(ICommandInfo commandInfo, IInteractionContext context, IResult result)
+    {
+        if (!result.IsSuccess)
+            switch (result.Error)
+            {
+                case InteractionCommandError.UnmetPrecondition:
+                    Console.WriteLine($"Not enough permissions to execute: {commandInfo.Name}");
+                    break;
+                case InteractionCommandError.UnknownCommand:
+                    Console.WriteLine($"Unknow command: {commandInfo.Name}.");
+                    break;
+                case InteractionCommandError.Exception:
+                    Console.WriteLine($"Exception while trying to run command: {commandInfo.Name}.");
+                    break;
+                default:
+                    Console.WriteLine($"An error has ocurred when processing: {commandInfo.Name}. Reason: {result.ErrorReason}");
+                    break;
+            }
+
+        return Task.CompletedTask;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,94 +1,59 @@
 ﻿using Discord;
-using Discord.Commands;
-using Discord.Net;
+using Discord.Interactions;
 using Discord.WebSocket;
-using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Globalization;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace LainDS
+namespace InteractionFramework;
+
+public class Program
 {
-    public class Program
+    private static IConfiguration _configuration;
+    private static IServiceProvider _services;
+
+    // Intents config
+    private static readonly DiscordSocketConfig _socketConfig = new()
     {
-        public static DiscordSocketClient? client;
+        GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers,
+        AlwaysDownloadUsers = true,
+    };
 
-        public static async Task Main(string[] args)
-        {
-            client = new DiscordSocketClient();
-            client.Log += Log;
-            client.Ready += Client_Ready;
-            client.SlashCommandExecuted += SlashCommandHandler;
+    // App login
+    public static async Task Main(string[] args)
+    {
+        _configuration = new ConfigurationBuilder()
+            .AddEnvironmentVariables(prefix: "?")
+            .AddJsonFile("appsettings.json", optional: true)
+            .Build();
 
+        _services = new ServiceCollection()
+            .AddSingleton(_configuration)
+            .AddSingleton(_socketConfig)
+            .AddSingleton<DiscordSocketClient>()
+            .BuildServiceProvider();
 
-            var token = Environment.GetEnvironmentVariable("TOKEN");
-            await client.LoginAsync(TokenType.Bot, token);
-            await client.StartAsync();
-            await Task.Delay(-1);
-        }
+        var client = _services.GetRequiredService<DiscordSocketClient>();
 
-        public static async Task Client_Ready()
-        {
-            var globalCommand = new SlashCommandBuilder();
-            globalCommand.WithName("idiot");
-            globalCommand.WithDescription("Terry Davis quote");
+        client.Log += LogAsync;
 
-            try
-            {
-                await client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
-            }
-            catch (HttpException exception)
-            {
-                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+        var token = Environment.GetEnvironmentVariable("TOKEN");
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
 
-                Console.WriteLine(json);
-            }
-        }
+        await client.SetCustomStatusAsync("Always connected.");
 
-        private static async Task SlashCommandHandler(SocketSlashCommand command)
-        {
-            switch (command.Data.Name)
-            {
-                case "idiot":
-                    await IdiotTerryDavisQuote(command);
-                    break;
-            }
-        }
+        await Task.Delay(Timeout.Infinite);
+    }
 
-        private static async Task IdiotTerryDavisQuote(SocketSlashCommand command)
-        {
-            await command.RespondAsync("An idiot admires complexity, a genius admires simplicity, " +
-                "a physicist tries to make it simple, for an idiot anything the more complicated it is the more he will admire it, " +
-                "if you make something so clusterfucked he can't understand it he's gonna think you're a god cause you made it so complicated nobody can understand it. " +
-                "That's how they write journals in Academics, they try to make it so complicated people think you're a genius.");
-        }
-
-        private static Task Log(LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
-        }
-
-        public class LoggingService
-        {
-            LoggingService(DiscordSocketClient client, CommandService command)
-            {
-                client.Log += LogAsync;
-                command.Log += LogAsync;
-            }
-
-            private Task LogAsync(LogMessage message)
-            {
-                if (message.Exception is CommandException cmdException)
-                {
-                    Console.WriteLine($"[Command/{message.Severity}] {cmdException.Command.Aliases[0]}" +
-                        $" failed to execute in {cmdException.Context.Channel}.");
-                    Console.WriteLine(cmdException);
-                }
-                else
-                {
-                    Console.WriteLine($"[General/{message.Severity}] {message}");
-                }
-
-                return Task.CompletedTask;
-            }
-        }
+    // Logs
+    private static Task LogAsync(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
     }
 }
